84d5e4ee5e29cc065c280c94bcb80f936547e547
Britt Lawson <brittlawsonrn@gmail.com>
2022-04-12 16:22:39 -0400

    scaffolded out pathway views with router links

--- vue/src/components/ChatBot.vue
+++ vue/src/components/ChatBot.vue
@@ -3,6 +3,7 @@
     <h1>ChatBot</h1>
     <h3>Hey there, {{ this.$store.state.nameOfCurrentUser }}!</h3>
     <h3>{{ this.$store.state.currentResponseObject.message }}</h3>
+    <h3>If that's not what you're looking for, try asking again and phrasing your question differently. </h3>
     <chat-bot-input />
   </div>
 </template>
