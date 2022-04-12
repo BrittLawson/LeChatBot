83e8ab9d85e30c092a80851e68815369a83f8dc3
Emily Sabo <eksabo@gmail.com>
2022-04-12 13:54:46 -0400

    Updated database schema to create tables and added Hit DAO

---
+++ java/src/main/java/com/techelevator/dao/JdbcHitDao.java
@@ -0,0 +1,64 @@
+package com.techelevator.dao;
+
+import com.techelevator.model.Hit;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class JdbcHitDao implements HitDao {
+    private final JdbcTemplate jdbcTemplate;
+
+    public JdbcHitDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public List<Hit> getHits(List<String> keywords) {
+        List<Hit> hitList = new ArrayList<>();
+
+        for(String word: keywords) {
+
+            String sql =
+            "SELECT DISTINCT topic, category, module_name, lesson_name, external_link_url, keyword" +
+            "FROM topic " +
+            "JOIN category USING (category_id) " +
+            "JOIN modules USING (module_id) " +
+            "JOIN lesson USING (lesson_id) " +
+            "JOIN external_link USING (external_link_id) " +
+            "JOIN topic_to_keyword USING (topic_id) " +
+            "JOIN keyword USING (keyword_id) " +
+            "WHERE keyword.keyword LIKE '%?%' ; ";
+
+
+            SqlRowSet results = jdbcTemplate.queryForRowSet(sql, word);
+
+            while (results.next()) {
+                Hit thisHit = mapRowToHit(results);
+                hitList.add(thisHit);
+            }
+
+        }
+        return hitList;
+    }
+
+    private Hit mapRowToHit(SqlRowSet rs) {
+        Hit hit = new Hit();
+
+        hit.setTopic(rs.getString("topic"));
+        hit.setCategory(rs.getString("category"));
+        hit.setModule(rs.getString("module_name")+ ", " +rs.getString("lesson_name") );
+        hit.setExternalUrl(rs.getString("external_link_url"));
+
+        return hit;
+    }
+
+
+}
+
+
+
+
---
+++ java/src/main/java/com/techelevator/model/Hit.java
@@ -0,0 +1,41 @@
+package com.techelevator.model;
+
+public class Hit {
+
+    private String topic;
+    private String category;
+    private String module;
+    private String externalUrl;
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getModule() {
+        return module;
+    }
+
+    public void setModule(String module) {
+        this.module = module;
+    }
+
+    public String getExternalUrl() {
+        return externalUrl;
+    }
+
+    public void setExternalUrl(String externalUrl) {
+        this.externalUrl = externalUrl;
+    }
+}
