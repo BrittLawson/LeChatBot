f059ef0915343172bb431d70e025daebfb640b57
Britt Lawson <brittlawsonrn@gmail.com>
2022-04-13 10:54:32 -0400

    populated all pathway info components with appropriate html content

--- vue/src/components/EmployerFollowUpInfo.vue
+++ vue/src/components/EmployerFollowUpInfo.vue
@@ -1,6 +1,175 @@
 <template>
   <div>
-      
+      <header>
+        <h1>Following Up After Interviews</h1>
+        <p>
+          The interview doesn't end once you leave or log off the Zoom call! Make sure you're taking the time 
+          to follow-up accordingly with the company and that you don't take your foot off the gas with other 
+          applications!
+        </p>
+      </header>
+      <section>
+        <h2>
+          Send a thank you
+        </h2>
+        <h3>Always send at thank you email (after each and every interview)</h3>
+        <ul>
+          <li>
+            Make it personal
+          </li>
+          <li>
+            Reiterate your interest in the role and company
+          </li>
+          <li>
+            Call out something specific from your conversation
+          </li>
+        </ul>
+      </section>
+      <section>
+        <h2>
+          Sending Thank You Notes After an Initial Phone Screen
+        </h2>
+        <div>
+          <h3>
+            Sample Thank You Message #1
+          </h3>
+          <p>
+            Subject Line: Thank You - Software Developer Interview
+          </p>
+          <p>
+           Hi Name, 
+          </p>
+          <p>
+            I appreciate having the opportunity to speak with you today about the Software Developer 
+            role at the ABCD company. The job seems to be a perfect match for my abilities and interests.
+          </p>
+          <p>
+            In addition to my enthusiasm, I would bring strong communication skills, flexibility, and the 
+            ability to encourage others to work cooperatively. During our conversation, I noted that at one 
+            point you stressed your need for a Junior Software Developer to quickly become a productive member 
+            of the team. Please know that I am more than happy to "go the extra mile" and can ensure that I 
+            would put in the extra work as needed to get up to speed.
+          </p>
+          <p>
+            I appreciate the time you took to interview me, and I look forward to hearing from you about the 
+            next steps in the hiring process.
+          </p>
+          <p>
+            Again, thank you very much for your time and consideration. I hope to hear from you soon.
+          </p>
+          <p>
+            Best Regards,
+          </p>
+          <p>
+            Jane/John Doe <br>
+            Email Address <br>
+            Phone Number
+          </p>
+        </div>
+        <div>
+          <h3>
+            Sample Thank You Message #2
+          </h3>
+          <p>
+            Subject Line: Thank You - John/Jane Doe Interview Junior Developer
+          </p>
+          <p>
+            Hi Name,
+          </p>
+          <p>
+            Thank you so much for meeting with me today to discuss the Junior Developer position at Tech Company. 
+            I really enjoyed our conversation, and believe my background in developing apps makes me a strong 
+            match for this position.
+          </p>
+          <p>
+            As I mentioned in our conversation, I've developed similar apps during my time at Tech Elevator, 
+            have studied several programming languages, and am comfortable working as part of a team to ship 
+            products on time and as bug-free as possible. While at Tech elevator, one of the apps that I helped 
+            develop as a side project received 2nd place at a Hackathon. My proven track record working on teams 
+            would be an asset to Tech Company, and I am very enthusiastic about this opportunity.
+          </p>
+          <p>
+            I look forward to scheduling the next steps in your process. Thank you again for the opportunity to 
+            speak today, and I hope to hear from you soon.
+          </p>
+          <p>
+            Sincerely,
+          </p>
+          <p>
+            John/Jane Doe <br>
+            Email Address <br>
+            Phone Number
+          </p>
+        </div>
+      </section>
+      <section>
+        <h2>
+          Sending Thank You Notes after a Final Round
+        </h2>
+        <div>
+          <h3>
+            Sample Thank You Message #1
+          </h3>
+          <p>
+            Subject Line: Thank You - Software Developer Interview
+          </p>
+          <p>
+            Hi First Name,
+          </p>
+          <p>
+            Thank you for connecting with me today to discuss the job opportunity at Company ABC.
+          </p>
+          <p>
+            I am very interested in the role and believe my skills would be an excellent match for the position 
+            as you described it. With my experience, I could immediately begin to contribute to your team as it 
+            expands.
+          </p>
+          <p>
+            On a personal note, I really enjoyed talking to you about our interest in cooking and the recent 
+            lasagna recipe you made for your family.
+          </p>
+          <p>
+            I look forward to hearing from you soon. Please don't hesitate to ask if there are any other 
+            questions I can answer about my work experience and how I might contribute to your company.
+          </p>
+          <p>
+            Best regards,
+          </p>
+          <p> 
+            John/Jane Doe <br>
+            Email Address <br>
+            Phone Number <br>
+          </p>
+        </div>
+        <div>
+          <h3>
+            Sample Thank You Message #2
+          </h3>
+          <p>
+            Subject Line: Thank You - John/Jane Doe Interview Junior Developer
+          </p>
+          <p>
+            Hi First Name,
+          </p>
+          <p>
+            It was such a pleasure to meet you today to discuss the Software Development role at XYZ Company. 
+            The position sounds exactly like the type of job I'm looking for, and I believe my experience with 
+            Java programming would help me thrive in the position.
+          </p>
+          <p>
+            If there are any additional questions I can answer, please let me know. Thanks again for meeting 
+            with me to speak about this exciting opportunity.
+          </p>
+          <p>
+            Sincerely,
+          </p>
+          <p>
+            John/Jane Doe <br>
+            Email Address <br>
+            Phone Number
+          </p>
+        </div>
+      </section>
   </div>
 </template>
 
